80307ce681ae522867bbbfbf35409580215250ce
Erwann Mest <m@kud.io>
2020-05-09 03:41:09 +0200

    Updates

--- _next/static/NefFaaZeO0HbWaeEnSLds/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/en/posts":[a],"/fr/posts":[a]}}("static\u002Fchunks\u002F361ae6b995b1bf4c0b09446b56a2236365c01c82.6e7c292770e1c16b3285.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/txXXXFNxlssgGA07MRKxW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/en/posts":[a],"/fr/posts":[a]}}("static\u002Fchunks\u002Fcbadea4698a82715f6082161bfb5be6c1e3a4d55.6e7c292770e1c16b3285.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
